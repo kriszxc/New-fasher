51e71ad476529a54f79ae687fb440baf615ca197
kriszxc <124692880+kriszxc@users.noreply.github.com>
2024-07-24 21:52:04 +0800

    Delete open_ai.py

--- open_ai.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client = OpenAI(api_key="sk-4NwYzHRgVbEY1ptK6dE21fF47dDc47C2Bf8324D32c36F5A6")
-
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role":"user","content":"四大文明古国分别有哪些",}
-    ]
-)
